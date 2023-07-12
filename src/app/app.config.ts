4bb9fb7dabc2cc7231d44bd23ae88f69358511ea
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-07-12 10:13:17 +0200

    Init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject } from '@angular/core';
+import { NavigationError, provideRouter, withDebugTracing, withEnabledBlockingInitialNavigation, withInMemoryScrolling, withNavigationErrorHandler, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [
+    provideRouter(routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+      }),
+      withEnabledBlockingInitialNavigation(),
+      withRouterConfig({
+        paramsInheritanceStrategy: 'always',
+        onSameUrlNavigation: 'reload'
+      }),
+      withDebugTracing(),
+      withNavigationErrorHandler((e: NavigationError) => inject(MyErrorTracker).trackError(e))
+    )
+  ]
 };
